7aed9b1e7d1cf8ea3c9e1e7517f433bc1144421d
Spitfire-Ice <37056025+Spitfire-Ice@users.noreply.github.com>
2019-09-14 19:14:47 +0300

    little england

--- src/cap.php
+++ src/cap.php
@@ -2,7 +2,8 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Your email address is: <?php echo $_POST["email"] <br>;
+ Notes is:<?php echo $__POST["notes"]?>?>
 
 </body>
 </html>
